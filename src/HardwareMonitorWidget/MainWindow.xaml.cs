caf7a94413df1f7ea1c88e4b2495d659520cc03b
Konstantin Kuzmin <kuzmin@meowpunk.com>
2026-02-13 12:59:52 +0300

    feat(ui): add minimal hardware metrics window

---
+++ src/HardwareMonitorWidget/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using HardwareMonitorWidget.Services;
+using HardwareMonitorWidget.ViewModels;
+
+namespace HardwareMonitorWidget;
+
+public partial class MainWindow : Window
+{
+    private readonly MainViewModel _viewModel;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var hardwareMonitorService = new LibreHardwareMonitorService();
+        var startupRegistrationService = new TaskSchedulerStartupRegistrationService();
+
+        _viewModel = new MainViewModel(hardwareMonitorService, startupRegistrationService);
+        DataContext = _viewModel;
+
+        Loaded += OnLoaded;
+        Closed += OnClosed;
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        await _viewModel.InitializeAsync();
+    }
+
+    private async void OnClosed(object? sender, EventArgs e)
+    {
+        Loaded -= OnLoaded;
+        Closed -= OnClosed;
+        await _viewModel.DisposeAsync();
+    }
+}
