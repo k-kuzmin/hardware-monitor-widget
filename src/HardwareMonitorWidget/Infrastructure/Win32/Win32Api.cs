c276489bbca908d4006a73f9c44728dc0822bde3
Konstantin Kuzmin <kuzmin@meowpunk.com>
2026-02-19 12:22:13 +0300

    refactor(step-1): extract Win32 P/Invoke to Infrastructure/Win32/Win32Api.cs
    Move DllImport signatures (SetWindowPos, MonitorFromPoint, GetMonitorInfo),
    structs (POINT, RECT, MONITORINFO) and constants (HWND_BOTTOM, SWP_*) out of
    MainWindow into a dedicated internal static class.

---
+++ src/HardwareMonitorWidget/Infrastructure/Win32/Win32Api.cs
@@ -0,0 +1,52 @@
+using System.Runtime.InteropServices;
+
+namespace HardwareMonitorWidget.Infrastructure.Win32;
+
+internal static class Win32Api
+{
+    public static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+    public const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+    public const uint SWP_NOSIZE   = 0x0001;
+    public const uint SWP_NOMOVE   = 0x0002;
+    public const uint SWP_NOACTIVATE = 0x0010;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr MonitorFromPoint(POINT pt, uint dwFlags);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public uint dwFlags;
+    }
+}
